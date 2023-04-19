53e81bec8286929d6584391f49e5672756accd06
DosyaSDU <doszhan_turar@mail.ru>
2023-04-19 17:53:38 +0600

    done

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        int remainder =month/3;
+        if (month < 1 || month > 12) {
+            System.out.println("wrong number");
+        }else if (remainder == 1 || remainder==4 ) {
+            System.out.println("Winter");
+        }else if (remainder == 2) {
+            System.out.println("Summer");
+        }else if (remainder == 3) {
+            System.out.println("Autumn");
+        }
     }
 }
